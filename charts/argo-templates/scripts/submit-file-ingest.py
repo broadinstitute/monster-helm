667551adf045d36f9eddc5ccf6fa368475f237f1
Drew Herbst <aherbst@broadinstitute.org>
2021-01-06 13:06:36 -0500

    Shift to using the bulkFileLoadArray method for ingesting a file, explicitly use named parameters rather than **kwargs

--- charts/argo-templates/scripts/submit-file-ingest.py
+++ charts/argo-templates/scripts/submit-file-ingest.py
@@ -5,21 +5,34 @@ import os
 
 credentials, project = google.auth.default(scopes=['openid', 'email', 'profile'])
 
-base_url = os.environ["API_URL"]
-dataset_id = os.environ["DATASET_ID"]
-profile_id = os.environ["PROFILE_ID"]
-source_path = os.environ["SOURCE_PATH"]
-target_path = os.environ["TARGET_PATH"]
-
 authed_session = AuthorizedSession(credentials)
 
 
-def ingest_file(dataset_id: str, **kwargs):
-    response = authed_session.post(f"{base_url}/api/repository/v1/datasets/{dataset_id}/files", json=kwargs)
+def ingest_file(api_url: str, dataset_id: str, profile_id: str, source_path: str, target_path: str):
+    body = {
+        "loadArray": [
+            {
+                "sourcePath": source_path,
+                "targetPath": target_path
+            }
+        ],
+        "maxFailedFileLoads": 0,
+        "profileId": profile_id
+    }
+    response = authed_session.post(f"{api_url}/api/repository/v1/datasets/{dataset_id}/files/bulk/array", json=body)
     if response.ok:
         return response.json()["id"]
     else:
         raise HTTPError(f"Bad response, got code of: {response.status_code}")
 
+
 # print the job id to std out
-print(ingest_file(dataset_id, profileId=profile_id, source_path=source_path, target_path=target_path))
+if __name__ == '__main__':
+    result = ingest_file(
+        api_url=os.environ["API_URL"],
+        dataset_id=os.environ["DATASET_ID"],
+        profile_id=os.environ["PROFILE_ID"],
+        source_path=os.environ["SOURCE_PATH"],
+        target_path=os.environ["TARGET_PATH"]
+    )
+    print(result)
