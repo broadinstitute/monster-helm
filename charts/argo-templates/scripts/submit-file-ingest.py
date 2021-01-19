70f36d3f51d38af7f9fef768605f9aa8a7e7307a
Drew Herbst <aherbst@broadinstitute.org>
2021-01-19 14:04:55 -0500

    Clean up

--- charts/argo-templates/scripts/submit-file-ingest.py
+++ charts/argo-templates/scripts/submit-file-ingest.py
@@ -3,12 +3,8 @@ from google.auth.transport.requests import AuthorizedSession
 from requests.exceptions import HTTPError
 import os
 
-credentials, project = google.auth.default(scopes=['openid', 'email', 'profile'])
 
-authed_session = AuthorizedSession(credentials)
-
-
-def ingest_file(api_url: str, dataset_id: str, profile_id: str, source_path: str, target_path: str):
+def ingest_file(session, base_url: str, dataset_id: str, profile_id: str, source_path: str, target_path: str):
     body = {
         "loadArray": [
             {
@@ -19,17 +15,22 @@ def ingest_file(api_url: str, dataset_id: str, profile_id: str, source_path: str
         "maxFailedFileLoads": 0,
         "profileId": profile_id
     }
-    response = authed_session.post(f"{api_url}/api/repository/v1/datasets/{dataset_id}/files/bulk/array", json=body)
+    response = session.post(f"{base_url}/api/repository/v1/datasets/{dataset_id}/files/bulk/array", json=body)
     if response.ok:
         return response.json()["id"]
     else:
         raise HTTPError(f"Bad response, got code of: {response.status_code}")
 
 
-# print the job id to std out
+def authed_session():
+    credentials, project = google.auth.default(scopes=['openid', 'email', 'profile'])
+    return AuthorizedSession(credentials)
+
+
 if __name__ == '__main__':
+    # print the job id to std out
     result = ingest_file(
-        api_url=os.environ["API_URL"],
+        base_url=os.environ["API_URL"],
         dataset_id=os.environ["DATASET_ID"],
         profile_id=os.environ["PROFILE_ID"],
         source_path=os.environ["SOURCE_PATH"],
